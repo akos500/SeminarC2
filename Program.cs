b6fef1fb84b840a250d4eccaa98407cc5732174a
akos500 <akos500@yandex.ru>
2022-09-23 18:12:49 +0300

    Вывод второго числа

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int meaning = number.ToString().Length;
+
+if (meaning < 3 || meaning > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+
+// Функция принимает сообщение для отображения в консоли
+// и выводит результат введенных данных 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция вывода цифры,находящейся в середине трехзначного числа
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
